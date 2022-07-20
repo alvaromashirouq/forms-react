fcaaa223ee19e6c49d4d9f073c438891ae7f85c0
Alvaro Martinez <alvaro.martinez@unosquare.com>
2022-07-20 13:11:41 -0500

    use hok with basic form

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,21 +1,91 @@
+import useInput from '../hooks/use-input';
+
 const BasicForm = (props) => {
+  const {
+    value: name,
+    isValid: nameIsValid,
+    hasError: namehasError,
+    valueBlurHandler: nameBlurHandler,
+    valueChangeHandler: nameChangeHandler,
+    reset: resetName
+  } = useInput((value) => value.trim() !== '');
+
+  const {
+    value: lastName,
+    isValid: lastNameisValid,
+    hasError: lastNamehasError,
+    valueBlurHandler: lastNameBlurHandler,
+    valueChangeHandler: lastNameChangeHandler,
+    reset: resetLastName
+  } = useInput((value) => value.trim() !== '');
+
+  const {
+    value: email,
+    isValid: emailIsValid,
+    hasError: emailhasError,
+    valueBlurHandler: emailBlurHandler,
+    valueChangeHandler: emailChangeHandler,
+    reset: resetEmail
+  } = useInput((value) => value.includes('@'));
+
+  let formIsValid = false;
+
+  if (nameIsValid && lastNameisValid && emailIsValid) {
+    formIsValid = true;
+  }
+
+  const onSubmissionHandler = (e) => {
+    e.preventDefault();
+    if (!formIsValid) {
+      return;
+    }
+
+    console.log('Submitted!');
+    console.log(name, lastName, email);
+
+    resetName();
+    resetLastName();
+    resetEmail();
+  };
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={onSubmissionHandler}>
+      <div className="control-group">
+        <div className="form-control">
+          <label htmlFor="name">First Name</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onBlur={nameBlurHandler}
+            onChange={nameChangeHandler}
+          />
+          {namehasError && <p>must be not empty</p>}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className="form-control">
+          <label htmlFor="name">Last Name</label>
+          <input
+            type="text"
+            id="name"
+            value={lastName}
+            onChange={lastNameChangeHandler}
+            onBlur={lastNameBlurHandler}
+          />
+          {lastNamehasError && <p>must be not empty</p>}
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">E-Mail Address</label>
+        <input
+          type="text"
+          id="name"
+          value={email}
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+        />
+        {emailhasError && <p>must be not empty</p>}
       </div>
-      <div className='form-actions'>
+      <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,53 @@
+import { useRef, useState } from 'react';
+import clsx from 'clsx';
+import useInput from '../hooks/use-input';
 const SimpleInput = (props) => {
+  const nameInputref = useRef('');
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const nameInputHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const nameInputBlurHandler = (e) => {
+    setEnteredNameTouched(true);
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+  };
+
+  const onSubmissionHandler = (e) => {
+    e.preventDefault();
+    setEnteredNameTouched(true);
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+    const enteredValue = nameInputref.current.value;
+
+    setEnteredName('');
+  };
+
+  const nameInputClasses = clsx(
+    enteredNameIsValid ? 'form-control' : 'form-control invalid'
+  );
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={onSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputref}
+          type="text"
+          id="name"
+          onBlur={nameInputBlurHandler}
+          onChange={nameInputHandler}
+        />
+        {!enteredNameIsValid && (
+          <p className="error-text">name must not be empty</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
